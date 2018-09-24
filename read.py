31d123f7790767c56ef1963041fa670e60654ac0
Ricky Tsai <r7i7c7k7y@gmail.com>
2018-09-24 20:16:19 +0800

    calc avg reviews

--- read.py
+++ read.py
@@ -6,7 +6,13 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
-print(data[0])
-print('--------------------')
-print(data[1])
+print('檔案讀取完了，總共有',len(data),'筆資料')
+
+sum = 0
+for x in data:
+	i = len(x)
+	sum = sum + i
+print(sum/len(data))
+
+
+
